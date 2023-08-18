fc5536656ee13ad7ba6dc0383d35fd19bf48a346
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-08-17 18:23:57 -0700

    made my load envirment file, updated git ignore file, established my server

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnviroment.mjs";
+import mongoose from "mongoose";
+import 'dotenv/config' 
+import bodyParser from "body-parser";
+
+const PORT = process.env.PORT || 4000
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(express.json())
+
+const dbConnection = async () => {
+  await mongoose.connect(process.env.ATLAS_URI)
+ 
+  console.log(`Connected to Database successfully!`)
+}
+
+dbConnection();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port : ${PORT}`)
+})
