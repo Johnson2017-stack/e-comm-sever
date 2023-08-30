672b86efe84a98539dbf6e1c9c63998df619ba78
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-08-30 14:59:27 -0700

    formatted all my documents with prettier

--- models/customerinfo.js
+++ models/customerinfo.js
@@ -1,28 +1,28 @@
-import { Schema, model } from 'mongoose'
+import { Schema, model } from "mongoose";
 
 const customerSchema = new Schema({
-    name: {
-        type: String,
-    },
-    address: {
-        type: String,
-    },
-    phone: {
-        type: String,
-    },
-    cardNumber: {
-        type: String,
-        require: true,
-    },
-    expirationDate: {
-        type: String,
-        require: true,
-    },
-    cv: {
-        type: String,
-        require: true,
-    },
-})
-const customerinfo = model("customerinfo", customerSchema)
+  name: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  cardNumber: {
+    type: String,
+    require: true,
+  },
+  expirationDate: {
+    type: String,
+    require: true,
+  },
+  cv: {
+    type: String,
+    require: true,
+  },
+});
+const customerinfo = model("customerinfo", customerSchema);
 
-export default customerinfo
+export default customerinfo;
--- server.js
+++ server.js
@@ -2,53 +2,48 @@ import express from "express";
 import cors from "cors";
 import "./loadEnviroment.mjs";
 import mongoose from "mongoose";
-import 'dotenv/config' 
+import "dotenv/config";
 import bodyParser from "body-parser";
 import customerinfo from "./models/customerinfo.js";
 
 const PORT = process.env.PORT || "";
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const dbConnection = async () => {
-  await mongoose.connect(process.env.ATLAS_URI)
- 
-  console.log(`Connected to Database successfully!`)
-}
+  await mongoose.connect(process.env.ATLAS_URI);
+
+  console.log(`Connected to Database successfully!`);
+};
 
 dbConnection();
 
 app.post("/api/payment", (req, res) => {
-    const customer = new customerinfo (
-        {
-            name: req.body.name,
-            address: req.body.address,
-            phone: req.body.phone,
-            cardNumber: req.body.cardNumber,
-            expirationDate: req.body.expirationDate,
-            cv: req.body.cv
-        }
-    )
-    customer.save()
-    .then((result) => {
-        res.status(200).send ({
-            result, 
-        })
-    })
-})
+  const customer = new customerinfo({
+    name: req.body.name,
+    address: req.body.address,
+    phone: req.body.phone,
+    cardNumber: req.body.cardNumber,
+    expirationDate: req.body.expirationDate,
+    cv: req.body.cv,
+  });
+  customer.save().then((result) => {
+    res.status(200).send({
+      result,
+    });
+  });
+});
 app.get("/api/customer", cors(), (req, res) => {
-    customerinfo.find({}).then(customers => {
-        res.status(200).json(customers)
-    })
-})
-
-
+  customerinfo.find({}).then((customers) => {
+    res.status(200).json(customers);
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`)
-})
+  console.log(`Server is running on port : ${PORT}`);
+});
